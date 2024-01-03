984dbd9204a052b3a30f3412f693421a282f1d81
Cabin Zhu <cabin_zhu@foxmail.com>
2024-01-03 17:11:50 +0800

    [New] Moved valid_set_cols() to common.py
    And added common.valid_set_num()

--- analysis.py
+++ analysis.py
@@ -11,22 +11,9 @@ def update_analysis(df_data):
     return df_data
 
 
-def _valid_set_cols():
-    """Get column names of set specified by global variables MIN_SET_NUM and MAX_SET_NUM.
-
-    E.g. MIN_SET_NUM = 1, MAX_SET_NUM = 2
-    -> Return [('weight_1', 'reps_1'), ('weight_2', 'reps_2')]
-    """
-    ret = []
-    for i in range(MIN_SET_NUM, MAX_SET_NUM + 1):
-        col_weight, col_reps = f'weight_{i}', f'reps_{i}'
-        ret.append((col_weight, col_reps))
-    return ret
-
-
 def update_capacity(df_data):
     df_data.loc[:, (COL_TOT_CAPACITY, COL_COMPLETED_SET_CAPACITY, COL_FULL_SET_CAPACITY)] = 0
-    for col_weight, col_reps in _valid_set_cols():
+    for col_weight, col_reps in valid_set_cols():
         df_data = df_data.fillna({col_weight: 0, col_reps: 0})
         c = df_data[col_weight] * df_data[col_reps]
         df_data[COL_TOT_CAPACITY] += c
@@ -41,7 +28,7 @@ def update_max_set(df_data, min_reps_per_set=SET_THRESHOLD):
     df_data[COL_MIN_SET_W] = float('inf')
 
     # Iterate over the valid set columns
-    for col_weight, col_reps in _valid_set_cols():
+    for col_weight, col_reps in valid_set_cols():
         # Select rows where the number of reps is greater than or equal to the minimum threshold
         cond_completed_set = df_data[col_reps] >= min_reps_per_set
         # Update the max weight for these rows
--- common.py
+++ common.py
@@ -15,3 +15,25 @@ MIN_SET_NUM = 1  # The min sequence number of sets that the visualization takes 
 MAX_SET_NUM = 4  # The max sequence number of sets that the visualization takes into account
 
 TEST_FILE_PATH = r'test/file/path'
+
+
+def valid_set_num():
+    """Get column names of set specified by global variables MIN_SET_NUM and MAX_SET_NUM.
+
+    E.g. MIN_SET_NUM = 2, MAX_SET_NUM = 4
+    -> Return [2, 3, 4]
+    """
+    return list(range(MIN_SET_NUM, MAX_SET_NUM + 1))
+
+
+def valid_set_cols():
+    """Get column names of set specified by global variables MIN_SET_NUM and MAX_SET_NUM.
+
+    E.g. MIN_SET_NUM = 1, MAX_SET_NUM = 2
+    -> Return [('weight_1', 'reps_1'), ('weight_2', 'reps_2')]
+    """
+    ret = []
+    for i in valid_set_num():
+        col_weight, col_reps = f'weight_{i}', f'reps_{i}'
+        ret.append((col_weight, col_reps))
+    return ret
